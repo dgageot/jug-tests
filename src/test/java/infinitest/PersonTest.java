bff2f8885130a911284b7c5a0ae1b9f87dcc4cbc
David Gageot <david@gageot.net>
2011-01-11 12:13:56 +0100

    Add a few tips on what to look at next

--- src/test/java/infinitest/PersonTest.java
+++ src/test/java/infinitest/PersonTest.java
@@ -1,8 +1,11 @@
 package infinitest;
 
 import static org.fest.assertions.Assertions.*;
-import org.junit.Test;
+import org.junit.*;
 
+/**
+ * Take a look at configuration files infinitest.args and infinitest.filters.
+ */
 public class PersonTest {
 	@Test
 	public void canCreatePerson() {
@@ -13,6 +16,7 @@ public class PersonTest {
 	}
 
 	@Test
+	@Ignore
 	public void canMakeInfinitestFail() {
 		Person person = new Person("David", 35);
 
